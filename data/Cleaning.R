e8f9b5c3c5493cdb1fc305f1c1bf47299d4ca41f
JonathanHOrtiz-Candelaria <jhoc@uw.edu>
2022-12-04 20:03:47 -0800

    Cleaning and proposal edit

--- data/Cleaning.R
+++ data/Cleaning.R
@@ -4,8 +4,10 @@ library(tidyverse)
 library(dplyr)
 wastate_deaths <- read.csv("../data/wastate_fetaldeaths.csv")
 View(wastate_deaths)
+maternalMortalityRatio <- read.csv("../data/maternalMortalityRatio.csv")
+View(maternalMortalityRatio)
 
-# Renaming columns 
+# Renaming columns: wastate_deaths
 wastate_deaths <- wastate_deaths %>% rename(year = StatOverview_Full_Data_data)
 wastate_deaths <- wastate_deaths %>% rename(fd_annotate = X)
 wastate_deaths <- wastate_deaths %>% rename(fd_suppress = X.1)
@@ -23,22 +25,42 @@ wastate_deaths <- wastate_deaths %>% rename(infantdeath_staterate = X.12)
 wastate_deaths <- wastate_deaths %>% rename(infant_deaths = X.13)
 wastate_deaths <- wastate_deaths %>% rename(live_births = X.14)
 
-# Removing unwanted columns 
+# Renaming columns: maternalMortalityRatio
+maternalMortalityRatio <- maternalMortalityRatio %>% rename(Country = Location)
+maternalMortalityRatio <- maternalMortalityRatio %>% rename(Year = Period)
+maternalMortalityRatio <- maternalMortalityRatio %>% rename(Ratio = First.Tooltip)
+
+# Removing unwanted columns: wastate_deaths
 wastate_deaths <- wastate_deaths[, -c(2, 3, 5, 6, 7, 8)] # removes fd_annotate, 
 # fd_suppress, id_annotate, id_ratelower, id_rateupper, and
 # id_suppress
 # link: https://sparkbyexamples.com/r-programming/remove-column-in-r/#:~:text=2.1%20Remove%20Column%20by%20Index&text=This%20notation%20takes%20syntax%20df,the%20%E2%80%93%20(negative)%20operator.
 
-# Removing unwanted rows
+# Removing unwanted columns: maternalMortalityRatio
+maternalMortalityRatio <- maternalMortalityRatio[, -c(3)]
+
+# Removing unwanted rows: wastate_deaths
 wastate_deaths <- wastate_deaths %>% filter(!row_number() %in% c(1, 2)) # removes incorrect year and 1990 rows 
 # link: https://www.r-bloggers.com/2022/06/remove-rows-from-the-data-frame-in-r/ 
 
-# Getting rid of all empty cells and replacing them with NA
+# Getting rid of all empty cells and replacing them with NA: wastate_deaths
 wastate_deaths[wastate_deaths == "" | wastate_deaths == " "] <- NA  # Replace blank & space by NA
 View(wastate_deaths) 
 # link: https://statisticsglobe.com/replace-blank-by-na-in-r 
 
-# Checking value types 
+# Removing the ranges from the Ratio column: maternalMortalityRatio
+num <- maternalMortalityRatio$Ratio
+get_num <- function() {
+  temp_num <- str_replace(num, " \\[.*\\]", "")
+  return(temp_num)
+}
+exact_num <- get_num()
+
+correct_deaths <- as.numeric(exact_num)
+
+maternalMortalityRatio$Ratio <- correct_deaths
+
+# Checking value types: wastate_deaths
 typeof(wastate_deaths$year)
 typeof(wastate_deaths$location)
 typeof(wastate_deaths$num_of_records)
@@ -51,7 +73,7 @@ typeof(wastate_deaths$infant_deaths)
 typeof(wastate_deaths$live_births)
 # all output as characters 
 
-# Changing value types from character to integer 
+# Changing value types from character to integer: wastate_deaths
 wastate_deaths$year <- as.integer(wastate_deaths$year)
 typeof(wastate_deaths$year) # integer 
 
@@ -83,3 +105,5 @@ typeof(wastate_deaths$live_births) # integer
 ?write.csv
 write.csv(wastate_deaths, file = "wastate_cleaned.csv")
 
+# Creating a new clean csv of maternalMortalityRatio
+write.csv(maternalMortalityRatio, file = "maternalMortalityRatio_cleaned.csv")
